76abdcbf5fa7f4ddfc0414332bd84c9f3180b49e
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-02 15:27:08 -0500

    "Task 2 - Sales Report."

--- cc_4.js
+++ cc_4.js
@@ -9,6 +9,17 @@ if (purchaseamount > 100) {
 }
 console.log(`Final price after discount: $${finalprice}`);
 
+// "Task 2 - Sales Report."
+
+let sales = [120, 85, 200, 150, 90]
+let totalsales = 0
+for (let index = 0; index < sales.length; index++) {
+    totalsales += sales[index];
+    
+}
+console.log (`Total Sales: $${totalsales}`);
+
+
 
 
 
