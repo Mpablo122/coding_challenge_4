e458a13c790ef954ad62b6619a5792299890ad63
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-02 15:20:20 -0500

    "Task 1 - Customer Discounts."

--- cc_4.js
+++ cc_4.js
@@ -3,11 +3,11 @@ let purchaseamount = 200
 let discounted_amount = purchaseamount;
 if (purchaseamount > 100) {
     let discounted_amount = purchaseamount * 0.2;
-    finalamount = purchaseamount - discounted_amount; //get finalized amount
+    finalprice = purchaseamount - discounted_amount; //get finalized amount
     console.log("20% off")
 
 }
-console.log(`Final amount after discount: $${finalamount}`);
+console.log(`Final price after discount: $${finalprice}`);
 
 
 
